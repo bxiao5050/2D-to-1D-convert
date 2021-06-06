6475fb172d3cef7d04e6802da0520bb954bf0310
= <bxiao5050@gmail.com>
2021-06-06 20:06:40 +0200

    fix GUI cannot show bug

--- automation.py
+++ automation.py
@@ -22,18 +22,10 @@ class Automation(Frame):
         self.gui.stopB.config(command = self.on_terminate)
 
 
-    def executionTime(self):
-        start = time.time()
-        while True:
-            if self.terminate_flag == False:
-                self.gui.countL3.config(text = f'execution time: {self.convert(time.time() - start)}' , fg = 'gray')
-            else:
-                return
     def on_terminate(self):
         self.terminate_flag = True
 
 
-                
     def on_start(self):
         self.gui.startB.config(state = 'disabled')
         self.gui.folderPath.config(state = 'disabled')
@@ -47,6 +39,38 @@ class Automation(Frame):
 
         self.xy_filename = ''
         self.runInf = ''
+
+
+        try:
+            threading.Thread(target=self.run).start()
+            threading.Thread(target=self.executionTime).start()
+            threading.Thread(target=self.display).start()
+        except:
+            self.terminate_flag = True
+            self.gui.startB.config(state = 'normal')
+            self.gui.folderPath.config(state = 'normal')
+            self.gui.pauseTime.config(state = 'normal')
+            self.gui.countL1.config(text = f'(stopped)    {self.finished_frames} data are finished', fg = 'red')
+
+    def executionTime(self):
+        start = time.time()
+        while True:
+            if self.terminate_flag == False:
+                self.gui.countL3.config(text = f'execution time: {self.convert(time.time() - start)}' , fg = 'gray')
+            else:
+                return
+    def display(self):
+           while True:
+            if self.terminate_flag == False:
+                suffix = ''
+                for i in range(3):
+                    suffix += '.'
+                    for j in range(40):
+                        self.gui.countL2.config(text =  f'{self.runInf}' + suffix, fg = 'gray')
+                        time.sleep(0.02)
+
+            else:
+                return
 
     def run(self):
         pauseTime = float(self.gui.pauseTime.get())
@@ -74,27 +98,84 @@ class Automation(Frame):
                 self.gui.countL1.config(text = f'total data: {total_frames}         finished: {self.finished_frames}' ,fg = 'black')
                 continue
 
+            self.gui.countL1.config(text = f'total data: {total_frames}         finished: {self.finished_frames}',fg = 'black')
+            self.oneround(frameblock, pauseTime)
+            iteration_N = 0 # record the iteration times
+            stableRunTime += 1
+            if stableRunTime >=2 and pauseTime >1:
+                pauseTime -= (0.07 + random.random()/20)
+                self.gui.pauseTime.config(state = 'normal')
+                self.gui.pauseTime.delete(0, 'end')
+                self.gui.pauseTime.insert(0, pauseTime)
+                self.gui.pauseTime.config(state = 'disabled')
+                stableRunTime = 0
+            # trialTimes = 0 # how many times did I try
+            while True:
+                if self.terminate_flag == True:
+                    self.gui.countL1.config(text = f'(stopped)    {self.finished_frames} data are finished', fg = 'red')
+                    self.initialization()
+                    return
+                iteration_N +=1
+                time.sleep(0.02)
+                # if pauseTime > 10:
+                #     pauseTime = 5
+                #     self.gui.pauseTime.delete(0, 'end')
+                #     self.gui.pauseTime.insert(0, pauseTime)
+                if os.path.exists(os.path.join(self.workPath, self.xy_filename)):
+                    self.finished_frames +=1
+                    self.runInf  =  f'obtained "{self.xy_filename_base}"'
+                    self.gui.log.see('end')
+                    self.gui.log.insert('end', f'{self.finished_frames} ---> obtained   "{self.xy_filename}"'+'\n')
+                    self.gui.countL1.config(text = f'total data: {total_frames}         finished: {self.finished_frames}' ,fg = 'black')
+                    break
+                if iteration_N > 100 and iteration_N < 106: #re-do it after waiting for 5s
+                    self.runInf  =  f'failed to obtain "{self.xy_filename_base}", try again'
+                    pyautogui.typewrite(['enter','esc','esc'])
+                    time.sleep(pauseTime/50)
+                    pyautogui.typewrite(['enter','esc','esc'])
+                    time.sleep(pauseTime/50)
+                    pyautogui.typewrite(['enter','esc','esc'])
+                    pauseTime += 0.3 + random.random()/7
+                    stableRunTime = 0
+                    self.gui.pauseTime.config(state = 'normal')
+                    self.gui.pauseTime.delete(0, 'end')
+                    self.gui.pauseTime.insert(0, pauseTime)
+                    self.gui.pauseTime.config(state = 'disabled')
+                    self.oneround(frameblock, pauseTime)
+                elif iteration_N >= 106 and iteration_N <108:
+                    self.runInf  =  f'failed to obtain "{self.xy_filename_base}", increase time and try again'
+                    pyautogui.typewrite(['enter','esc','esc'])
+                    time.sleep(pauseTime/50)
+                    pyautogui.typewrite(['enter','esc','esc'])
+                    time.sleep(pauseTime/50)
+                    pyautogui.typewrite(['enter','esc','esc'])
+                    pauseTime = 5
+                    stableRunTime = 0
+                    self.gui.pauseTime.config(state = 'normal')
+                    self.gui.pauseTime.delete(0, 'end')
+                    self.gui.pauseTime.insert(0, pauseTime)
+                    self.gui.pauseTime.config(state = 'disabled')
+                elif iteration_N >= 108 :
+                    self.gui.log.see('end')
+                    self.gui.log.insert('end', f'"{self.xy_filename}" failed'+'\n')
+                    break
 
         self.initialization()
         if total_frames == self.finished_frames:
             self.gui.countL1.config(text = f'all {total_frames} data are finished', fg = 'blue')
 
+    def initialization(self):
+        self.terminate_flag = True
+        self.gui.startB.config(state = 'normal')
+        self.gui.folderPath.config(state = 'normal')
+        self.gui.pauseTime.config(state = 'normal')
+
+
+    def convert(self, seconds):
+        return time.strftime("%H:%M:%S", time.gmtime(seconds))
+
+
     def oneround(self, frameblock, pauseTime= 5):
-        for frameblock in frames['diffName']:
-            if self.terminate_flag == True:
-                self.gui.countL1.config(text = f'(stopped)    {self.finished_frames} data are finished', fg = 'red')
-                self.initialization()
-                return
-            self.xy_filename_base = frameblock.split('.gfrm')[0].replace('"','')
-            self.xy_filename = self.xy_filename_base + '_exported.xy'
-            self.runInf  =  f'convert "{self.xy_filename_base}"'
-            #check if the 1D pattern already exists
-            if os.path.exists(os.path.join(self.workPath, self.xy_filename)):
-                self.finished_frames +=1
-                self.gui.log.see('end')
-                self.gui.log.insert('end', f'{self.finished_frames} -- exists   "{self.xy_filename}"'+'\n')
-                self.gui.countL1.config(text = f'total data: {total_frames}         finished: {self.finished_frames}' ,fg = 'black')
-                continue
         if self.getFocused == False:
             pyautogui.hotkey('winleft', '1')
             self.getFocused = True
@@ -107,23 +188,6 @@ class Automation(Frame):
         pyautogui.hotkey('ctrlleft', 'i')
         time.sleep(pauseTime/2)
 
-        self.runInf  =  f'save 1D diffractio pattern as ".xy"'
-        pyautogui.typewrite(['tab'])
-        time.sleep(pauseTime/7)
-        pyautogui.typewrite(['down'])
-        time.sleep(pauseTime/4)
-        pyautogui.typewrite([ 'down'])
-        time.sleep(pauseTime/30)
-        pyautogui.typewrite([ 'down'])
-        time.sleep(pauseTime/30)
-        pyautogui.typewrite(['enter'])
-        time.sleep(pauseTime/10)
-        pyautogui.typewrite([ 'enter'])
-        time.sleep(pauseTime/5
-            )
-
-
-        pyautogui.typewrite(['left', 'enter'])
         # print(path)
 
         if self.isFirstRound == True:
@@ -156,11 +220,48 @@ class Automation(Frame):
         pyautogui.typewrite(['up', 'up', 'up', 'right', 'down', 'down', 'enter'])
         time.sleep(pauseTime/5)
 
+        self.runInf  =  f'save 1D diffractio pattern as ".xy"'
+        pyautogui.typewrite(['tab'])
+        time.sleep(pauseTime/7)
+        pyautogui.typewrite(['down'])
+        time.sleep(pauseTime/4)
+        pyautogui.typewrite([ 'down'])
+        time.sleep(pauseTime/30)
+        pyautogui.typewrite([ 'down'])
+        time.sleep(pauseTime/30)
+        pyautogui.typewrite(['enter'])
+        time.sleep(pauseTime/10)
+        pyautogui.typewrite([ 'enter'])
+        time.sleep(pauseTime/5
+            )
 
 
-    def convert(self, seconds):
-        return time.strftime("%H:%M:%S", time.gmtime(seconds))
-    
+        pyautogui.typewrite(['left', 'enter'])
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    root = Tk()
+    root.title('convert 2D XRD to 1D pattern')
+    app = Automation(root)
+    app.pack()
+
+    root.mainloop()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 
--- formattedScanlist.py
+++ formattedScanlist.py
@@ -14,67 +14,18 @@ class FormattedScanlist():
 
         self.workPath = askdirectory() if workPath is None else workPath
 
-    def _getBlock(self, scanLists):
-        frames = {'diffNum':[], 'diffName':[]}
 
+        # block = self.block()
+        # print(self.frames())
+        # print(block['filenames'][0])
+        # print(block)
 
-        for v in scanLists.values():
-            frames['diffNum'].append(len(v))
-            frames['diffName'].append('>'.join(v))
+    def block(self):
+        return self._getBlock(self._scanLists())
 
-        block = {'filenum':[], 'filenames':[]}
-        index = 0
-        while index <= len(frames['diffNum']):
-            filenum = min([len(frames['diffNum'])-index, self.scan_n])
-            block['filenum'].append(filenum)
-            s = '>'.join([v for v in frames['diffName'][index:filenum+index]])
-            ss =  ' '.join(['"{}"'.format(c) for c in s.split('>')])
-            block['filenames'].append(ss)
-            index += self.scan_n
-        return block
-    def _scanLists(self):
-
-        files = glob.glob(os.path.join(self.workPath, '*.gfrm'))
-
-        #sorted file names in a given directory
-        filenames = sorted([os.path.basename(f) for f in files])
-        scanLists = defaultdict(list)
-        for f in filenames:
-            scanLists[f[0:-8]].append(f)
-        return scanLis
-        
-    def _getBlock(self, scanLists):
-        frames = {'diffNum':[], 'diffName':[]}
-
-
-        for v in scanLists.values():
-            frames['diffNum'].append(len(v))
-            frames['diffName'].append('>'.join(v))
-
-        block = {'filenum':[], 'filenames':[]}
-        index = 0
-        while index <= len(frames['diffNum']):
-            filenum = min([len(frames['diffNum'])-index, self.scan_n])
-            block['filenum'].append(filenum)
-            s = '>'.join([v for v in frames['diffName'][index:filenum+index]])
-            ss =  ' '.join(['"{}"'.format(c) for c in s.split('>')])
-            block['filenames'].append(ss)
-            index += self.scan_n
-        return block
-        
-
-        
-    def _scanLists(self):
-
-        files = glob.glob(os.path.join(self.workPath, '*.gfrm'))
-
-        #sorted file names in a given directory
-        filenames = sorted([os.path.basename(f) for f in files])
-        scanLists = defaultdict(list)
-        for f in filenames:
-            scanLists[f[0:-8]].append(f)
-        return scanLists  
-
+        """
+        {'diffNum': [3, 3, 2, 3, 3, 3, 1], 'diffName': ['191106-K2-2 V6-IL-Ag-Ir-Pd-Pt-Ru_400_deg_001.gfrm>191106-K2-2 V6-IL-Ag-Ir-Pd-Pt-Ru_400_deg_002.gfrm>191106-K2-2 V6-IL-Ag-Ir-Pd-Pt-Ru_400_deg_003.gfrm',
+        """
     def frames(self):
         scanLists = self._scanLists()
         frames = {'diffNum':[], 'diffName':[]}
@@ -83,5 +34,50 @@ class FormattedScanlist():
             frames['diffName'].append(' '.join(['"{}"'.format(c) for c in v]))
         return frames
 
+    def _getBlock(self, scanLists):
+        frames = {'diffNum':[], 'diffName':[]}
 
 
+        for v in scanLists.values():
+            frames['diffNum'].append(len(v))
+            frames['diffName'].append('>'.join(v))
+
+        block = {'filenum':[], 'filenames':[]}
+        """
+        {'filenum': [4, 3], 'filenames': ['191106-K2-2 V6-IL-Ag-Ir-Pd-Pt-Ru_400_deg_001.gfrm>191106-K2-2
+        """
+        index = 0
+        while index <= len(frames['diffNum']):
+            filenum = min([len(frames['diffNum'])-index, self.scan_n])
+            block['filenum'].append(filenum)
+            s = '>'.join([v for v in frames['diffName'][index:filenum+index]])
+            ss =  ' '.join(['"{}"'.format(c) for c in s.split('>')])
+            block['filenames'].append(ss)
+            index += self.scan_n
+        return block
+
+
+    def _scanLists(self):
+
+        files = glob.glob(os.path.join(self.workPath, '*.gfrm'))
+
+        #sorted file names in a given directory
+        filenames = sorted([os.path.basename(f) for f in files])
+        scanLists = defaultdict(list)
+        for f in filenames:
+            scanLists[f[0:-8]].append(f)
+        return scanLists
+
+
+
+
+def main():
+    root = Tk()
+    app = FormattedScanlist()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
